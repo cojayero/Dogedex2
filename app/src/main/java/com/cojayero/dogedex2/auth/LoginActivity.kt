cc358df7e9b96e794b03b1fd722739d8239abacd
Baldomero Fernandez <f_baldomero@hotmail.com>
2022-06-08 20:10:46 +0200

    Added fragments  layout for login and register

---
+++ app/src/main/java/com/cojayero/dogedex2/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.cojayero.dogedex2.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cojayero.dogedex2.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
